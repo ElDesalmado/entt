ce1c8c0f955bee2c116fec3dfe6b7bfd0a866704
s.kolesnik <7fosGbc9S>
2021-06-25 11:24:10 +0500

    separated implementation for type_seq into a standalone header. Added macros to configure separate compilation and dynamic building

--- src/entt/core/impl/type_info.ipp
+++ src/entt/core/impl/type_info.ipp
@@ -1,8 +1,6 @@
 #ifndef ENTT_TYPE_INFO_IPP
 #define ENTT_TYPE_INFO_IPP
 
-#include "entt/core/type_info.hpp"
-
 namespace entt {
 
 namespace internal {
--- src/entt/impl/src.hpp
+++ src/entt/impl/src.hpp
@@ -1,15 +1,15 @@
 #ifndef ENTT_SRC_HPP
 #define ENTT_SRC_HPP
 
+#ifndef ENTT_IMPL_SRC_HPP
+#define ENTT_IMPL_SRC_HPP
 
 #define ENTT_SOURCE
 
 #include "entt/config/config.h"
 
 #if defined(ENTT_HEADER_ONLY)
-# error Do not compile EnTT library source with ENTT_HEADER_ONLY defined
+# error Do not compile Asio library source with ENTT_HEADER_ONLY defined
 #endif
-
-#include "entt/core/impl/type_info.ipp"
 
 #endif // ENTT_SRC_HPP
